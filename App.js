5691e25bd2f6ef47b705fa96106954f35f6ff316
kikiolivia <kikiolivia54@gmail.com>
2020-03-10 16:30:24 +0700

    lesson-1

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+
+  const [name, setName] = useState('shaun');
+  const [person, setPerson] = useState({ name: 'mario', age: 40 });
+
+  const clickHandler = () => {
+    setName('chun-li');
+    setPerson({ name: 'luigi', age: 45 });
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>My name is {name}</Text>
+      <Text>His name is {person.name} and his age is {person.age}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="update state" onPress={clickHandler} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  }
+});
